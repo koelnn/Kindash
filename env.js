13e42cee30629da396fafc358b1c975ef808cede
koelnn <yiyaotong7@gmail.com>
2026-06-02 20:18:55 +0800

    no key

--- env.js
+++ env.js
@@ -2,5 +2,4 @@ window._env_ = window._env_ || {
   VIKA_TOKEN: 'uskpjOZKYkXL2lJWWzGfkF9',
   DATASHEET_ID: 'dstMlQqwPVwHk920bK',
   SUMMARY_DATASHEET_ID: 'dstMlQqwPVwHk920bK',
-  ACCESS_KEY: 'Kdash'
 };
